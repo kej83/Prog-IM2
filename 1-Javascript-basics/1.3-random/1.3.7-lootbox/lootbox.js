a3c26ba66950f0b76edb96e72b2625e6a1cd4a85
kej83 <kej83@pm.me>
2025-11-05 11:03:58 +0100

    losning lootbox

--- 1-Javascript-basics/1.3-random/1.3.7-lootbox/lootbox.js
+++ 1-Javascript-basics/1.3-random/1.3.7-lootbox/lootbox.js
@@ -1,3 +1,22 @@
 function openBox() {
-  // deres kode her
+    // deres kode her
+    // <p id="result"
+    // 0. Lag tilfeldig tall 0-1
+    let tall = Math.random();
+    let txt = "Du fikk en ";
+    // 1. Sjekk om legendary (2 %)
+    if (tall < 0.02) {
+        txt += "legendary item.";
+    } // Epic 8 %
+    else if (tall < 0.10) {
+        txt += "epic item.";
+    } // Rare 20 %
+    else if (tall < 0.30) {
+        txt += "rare item.";
+    } // common 70 %
+    else {
+        txt += "common item.";
+    }
+
+    document.getElementById("result").textContent = txt;
 }
